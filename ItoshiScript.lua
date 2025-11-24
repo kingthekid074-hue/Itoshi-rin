606c2a566312178c92623d9beaf602b09777eda0
kingthekid074-hue <kingthekid074@gmail.com>
2025-11-24 15:42:52 +0100

    Update ItoshiScript.lua

--- ItoshiScript.lua
+++ ItoshiScript.lua
@@ -1,4 +1,4 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "itoshi hub", HidePremium = false, SaveConfig = true, ConfigFolder = "itoshi config"})
-end
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({ Name = "itoshi hub",HidePremium = false,SaveConfig = true,ConfigFolder = "itoshi config"})
+local MainTab = Window:MakeTab({Name = "Main",Icon = "rbxassetid://4483345998",PremiumOnly = false})
 OrionLib:Init()
